47b6b112b31de3292fff9c1c0b449379ce3d8605
unknown-user <unknown-user@192.168.3.46>
2014-10-15 00:28:26 +0530

    Initial check-in of module TestApp_XS_VersionControl7

---
+++ TestApp_XS_VersionControl7/TestApp_XS_VersionControl7ViewController.designer.cs
@@ -0,0 +1,17 @@
+﻿//
+// This file has been generated automatically by MonoDevelop to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+
+namespace TestApp_XS_VersionControl7
+{
+	[Register ("TestApp_XS_VersionControl7ViewController")]
+	partial class TestApp_XS_VersionControl7ViewController
+	{
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
